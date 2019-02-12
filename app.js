f42ba8b35af4ac3d2d05d257dffa7c7a0e465df7
Himstar <issiaka_a@live.fr>
2019-02-12 15:01:02 +0100

    Setup Gaphql

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql'); // Middleware ready :=)
+
+const { buildSchema } = require('graphql');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float! 
+          date: String!
+        }
+
+        type RootQuery {
+          events: [Events!]!
+        }
+
+        type RootMutation {
+          createEvent(name: String): String
+        }
+
+        schema {
+          query: RootQuery
+          mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
